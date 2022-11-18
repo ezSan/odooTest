0b97ee47d3bb60e759dd6f0330935a172b7dd775
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-11-17 22:27:47 -0300

    added provincia at db & subtitle at cards

---
+++ scripts/saved.js
@@ -0,0 +1,42 @@
+let savedTitle = document.getElementById('savedTitle');
+let likeadasBox = document.getElementById('likeadasBox');
+let likes = JSON.parse(localStorage.getItem('Liked'))
+let cantidadLikes = likes.length;
+let dinamicP = document.createElement('p');
+dinamicP.classList.add('dinamicP');
+dinamicP.innerText = `Usted tiene ${cantidadLikes} publicaciones guardadas`;
+savedTitle.insertAdjacentElement('afterend', dinamicP);
+
+
+let createArticles =()=>{
+    likes.map( x=>{
+
+        let card = document.createElement('div');
+        likeadasBox.appendChild(card);
+        card.classList.add('card');
+        let img = document.createElement('img');
+        img.src= x.img;
+        card.appendChild(img);
+        let title = document.createElement('h5');
+        title.innerHTML = x.title;
+        title.classList.add('cardTitle');
+        img.insertAdjacentElement('afterend', title);
+        let provincia = document.createElement('p');
+        provincia.innerHTML= x.subtitle;
+        provincia.classList.add('cardSubtitle');
+        title.insertAdjacentElement('afterend', provincia);
+
+
+
+
+        
+        
+        
+
+
+        
+
+    })
+}
+
+createArticles()
